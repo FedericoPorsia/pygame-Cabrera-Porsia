24fef93b991fa403e75f73aa00c275a413783ae1
FedericoPorsia <149029033+FedericoPorsia@users.noreply.github.com>
2023-11-07 00:35:49 -0300

    cambios

--- principal.py
+++ principal.py
@@ -33,10 +33,11 @@ IMAGEN_PRODUCTO_HEIGHT = 50
 def dibujar_nave(rect):
     screen.blit(NAVE_ESPACIAL, (rect.x, rect.y))
 
+
 def manejar_movimiento_nave(tecla_presionada , rectangulo):
     if tecla_presionada[pygame.K_w] and rectangulo.y - VELOCIDAD_NAVE >= 100 and rectangulo.y - VELOCIDAD_NAVE <= 550: # Arriba
         rectangulo.y -= VELOCIDAD_NAVE
-    
+
     if tecla_presionada[pygame.K_s] and rectangulo.y - VELOCIDAD_NAVE >= 0 and rectangulo.y - VELOCIDAD_NAVE <= 500: # Abajo
         rectangulo.y += VELOCIDAD_NAVE
 
@@ -63,7 +64,7 @@ def dibujar_disparos(disparos):
     for disparo in disparos:
         pygame.draw.rect(screen, (255,0,0), disparo)
 
-def get_font(size): 
+def get_font(size):
     return pygame.font.Font("assets/font.ttf", size)
 
 def main():
@@ -80,9 +81,9 @@ def main():
     nivel = 1
     segundos = TIEMPO_MAX # Tiempo max va aca
     corriendo = True
-    puntos = 0 
+    puntos = 0
     producto_candidato = ""
-    lista_productos = lectura()  
+    lista_productos = lectura()
     producto = dameProducto(lista_productos, MARGEN)
     productos_en_pantalla = dameProductosAleatorios(producto, lista_productos, MARGEN)
     producto_elegido_programa = dameProducto(productos_en_pantalla[1:], MARGEN)
@@ -118,7 +119,7 @@ def main():
                 #     producto_candidato = producto_candidato[0:len(producto_candidato)-1]
                 # if e.key == K_RETURN:  # presionó enter
                 #     nivel += 1
-                   
+
                 #     if indice < len(productos_en_pantalla):
                 #         puntos += procesar(producto, productos_en_pantalla[indice], MARGEN)
                 #         producto_candidato = ""
@@ -135,11 +136,11 @@ def main():
                 if e.rect == diccionario[index][1]:
                     print("PRODUCTO ELEGIDO GOLPEADO!!!")
                     puntos += procesar(producto, productos_en_pantalla[index], MARGEN)
-                
+
                 producto = dameProducto(lista_productos, MARGEN)
                 productos_en_pantalla = dameProductosAleatorios(producto, lista_productos, MARGEN)
                 producto_elegido_programa = dameProducto(productos_en_pantalla[1:], MARGEN)
-                index = index_producto_elegido(productos_en_pantalla, producto_elegido_programa)    
+                index = index_producto_elegido(productos_en_pantalla, producto_elegido_programa)
         for i in lista_rectangulos_prod:
             manejar_disparos(disparos,i)
         tecla_presionada = pygame.key.get_pressed()
@@ -167,11 +168,11 @@ def menu():
         MENU_TEXT = get_font(30).render("PEGUELE AL PRECIO", True, "#b68f40")
         MENU_RECT = MENU_TEXT.get_rect(center=(400, 100))
 
-        PLAY_BUTTON = Button(None, pos=(400, 250), 
+        PLAY_BUTTON = Button(None, pos=(400, 250),
                             text_input="1 jugador", font=get_font(25), base_color="#d7fcd4", hovering_color="White")
-        OPTIONS_BUTTON = Button(None, pos=(400, 400), 
+        OPTIONS_BUTTON = Button(None, pos=(400, 400),
                             text_input="2 jugadores", font=get_font(25), base_color="#d7fcd4", hovering_color="White")
-        QUIT_BUTTON = Button(None, pos=(400, 550), 
+        QUIT_BUTTON = Button(None, pos=(400, 550),
                             text_input="Records", font=get_font(25), base_color="#d7fcd4", hovering_color="White")
 
         screen.blit(MENU_TEXT, MENU_RECT)
@@ -189,14 +190,161 @@ def menu():
                 if PLAY_BUTTON.chequearEntrada(MENU_MOUSE_POSICION):
                     instrucciones()
                     corriendo = False
-              
+                if OPTIONS_BUTTON.chequearEntrada(MENU_MOUSE_POSICION):
+                    instrucciones2()
+                    corriendo = False
+
+
                 if QUIT_BUTTON.chequearEntrada(MENU_MOUSE_POSICION):
                     records()
 
         if corriendo:
             pygame.display.update()
             fpsClock.tick(30)
-    
+
+def dibujar_nave2(rect):
+    screen.blit(NAVE_ESPACIAL, (rect.x, rect.y))
+
+def manejar_movimiento_nave2(tecla_presionada , rectangulo):
+    if tecla_presionada[pygame.K_t] and rectangulo.y - VELOCIDAD_NAVE >= 100 and rectangulo.y - VELOCIDAD_NAVE <= 550: # Arriba
+        rectangulo.y -= VELOCIDAD_NAVE
+
+    if tecla_presionada[pygame.K_g] and rectangulo.y - VELOCIDAD_NAVE >= 0 and rectangulo.y - VELOCIDAD_NAVE <= 500: # Abajo
+        rectangulo.y += VELOCIDAD_NAVE
+def main2():
+    rect = pygame.Rect(400, 300, NAVE_ESPACIAL_WIDTH,NAVE_ESPACIAL_HEIGHT)
+    rect1 = pygame.Rect(0, 300, NAVE_ESPACIAL_WIDTH,NAVE_ESPACIAL_HEIGHT)
+    lista_rectangulos_prod = dibujar_rect_productos()
+    diccionario = {}
+    contador = 0
+    for i in lista_rectangulos_prod:
+        diccionario[contador] = [i.x, i.y]
+        contador += 1
+    print(diccionario)
+    gameClock = pygame.time.Clock()
+    totaltime = 0
+    nivel = 1
+    segundos = TIEMPO_MAX # Tiempo max va aca
+    corriendo = True
+    puntos = 0
+    producto_candidato = ""
+    lista_productos = lectura()
+    producto = dameProducto(lista_productos, MARGEN)
+    productos_en_pantalla = dameProductosAleatorios(producto, lista_productos, MARGEN)
+    producto_elegido_programa = dameProducto(productos_en_pantalla[1:], MARGEN)
+    index = index_producto_elegido(productos_en_pantalla, producto_elegido_programa)
+    print(productos_en_pantalla)
+    dibujar1(screen, productos_en_pantalla, producto,
+            producto_candidato, puntos, segundos, nivel)
+    time_delay = 1000
+    timer_event = pygame.USEREVENT+1
+    pygame.time.set_timer(timer_event, time_delay)
+
+
+    while corriendo:
+        gameClock.tick(40)
+        totaltime += gameClock.get_time()
+
+        for e in pygame.event.get():
+
+            if e.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if e.type == timer_event:
+                segundos -= 1
+                if segundos == 0:
+                    juego_terminado(puntos)
+                    corriendo = False
+            if e.type == KEYDOWN:
+                # Hay un error si pones una letra por ejemplo y clickeas
+                # aunque no aparezca en  pantalla!
+                # letra = dameLetraApretada(e.key)
+                # producto_candidato += letra  # va concatenando las letras que escribe
+                # if e.key == K_BACKSPACE:
+                #     producto_candidato = producto_candidato[0:len(producto_candidato)-1]
+                # if e.key == K_RETURN:  # presionó enter
+                #     nivel += 1
+
+                #     if indice < len(productos_en_pantalla):
+                #         puntos += procesar(producto, productos_en_pantalla[indice], MARGEN)
+                #         producto_candidato = ""
+                #         producto = dameProducto(lista_productos, MARGEN)
+                #         productos_en_pantalla = dameProductosAleatorios(producto, lista_productos, MARGEN)
+                #     else:
+                #         producto_candidato = ""
+                if e.key == pygame.K_RCTRL and len(disparos) < MAXIMOS_DISPAROS:
+                    bullet = pygame.Rect(rect.x + rect.width, rect.y + rect.height // 2 - 2, 10, 5)
+                    disparos.append(bullet)
+            if e.type == DISPARO_IMPACTO:
+                print(diccionario[index][1])
+                nivel += 1
+                if e.rect == diccionario[index][1]:
+                    print("PRODUCTO ELEGIDO GOLPEADO!!!")
+                    puntos += procesar(producto, productos_en_pantalla[index], MARGEN)
+
+                producto = dameProducto(lista_productos, MARGEN)
+                productos_en_pantalla = dameProductosAleatorios(producto, lista_productos, MARGEN)
+                producto_elegido_programa = dameProducto(productos_en_pantalla[1:], MARGEN)
+                index = index_producto_elegido(productos_en_pantalla, producto_elegido_programa)
+        for i in lista_rectangulos_prod:
+            manejar_disparos(disparos,i)
+        tecla_presionada = pygame.key.get_pressed()
+        manejar_movimiento_nave(tecla_presionada, rect)
+        manejar_movimiento_nave2(tecla_presionada, rect1)
+
+        screen.fill(COLOR_FONDO)
+        screen.blit(BACKGROUND, (0, 0))
+        dibujar_nave(rect)
+        dibujar_nave2(rect1)
+
+        dibujar_disparos(disparos)
+        dibujar1(screen, productos_en_pantalla, producto,
+            producto_candidato, puntos, segundos, nivel)
+
+        pygame.display.update()
+
+def instrucciones2():
+
+    fpsClock = pygame.time.Clock()
+    pygame.display.flip()
+    font_titulo = pygame.font.Font('assets/font.ttf', 20)
+    font_texto = pygame.font.Font('assets/font.ttf', 14)
+    corriendo = True
+    while True:
+        screen.fill((0, 0, 0))
+        texto =  font_titulo.render("Instrucciones", True, (255, 255, 255))
+        screen.blit(texto, [250, 25])
+        archivo_instrucciones = open("./instrucciones2.txt")
+        x = 30
+
+        for linea in archivo_instrucciones:
+            linea = linea[0:-1]
+            texto = font_texto.render(linea, True, (255, 255, 255))
+            x+= 30
+            screen.blit(texto, [25, 100 + x])
+
+        texto =  font_titulo.render("TOQUE CUALQUIER TECLA PARA EMPEZAR", True, (255, 255, 255))
+        screen.blit(texto, [50, 500])
+
+        archivo_instrucciones.close()
+
+        fpsClock.tick(30)
+        pygame.display.flip()
+
+        pygame.time.wait(1000)
+        for event in pygame.event.get():
+            # Presionar cualquier tecla para ir al juego
+            if event.type == KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                main()
+                return
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+                corriendo == False
+
+        if corriendo:
+            pygame.display.update()
+
 def instrucciones():
     fpsClock = pygame.time.Clock()
     pygame.display.flip()
@@ -226,7 +374,7 @@ def instrucciones():
 
         pygame.time.wait(1000)
         for event in pygame.event.get():
-            # Presionar cualquier tecla para ir al juego 
+            # Presionar cualquier tecla para ir al juego
             if event.type == KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
                 main()
                 return
@@ -237,7 +385,6 @@ def instrucciones():
 
         if corriendo:
             pygame.display.update()
-            
 
 def juego_terminado(puntos):
     fpsClock = pygame.time.Clock()
@@ -250,10 +397,10 @@ def juego_terminado(puntos):
         screen.fill((0, 0, 0))
         texto = font.render("GAME OVER", True, (255,0,0), (180, 200, 200))
         screen.blit(texto, (200, 100))
-        
+
         texto = font2.render(f"Sus puntos fueron {str(puntos)}", True, (255, 255,255))
         screen.blit(texto, (ANCHO / 5, 200))
-        
+
         texto = font2.render(f"Ingrese un apodo", True, (255, 150,200))
         screen.blit(texto, (ANCHO / 3.5, 300))
 
@@ -266,7 +413,7 @@ def juego_terminado(puntos):
                      (350, 450), (310, 450), 5)
 
         for event in pygame.event.get():
-            # Presionar cualquier tecla para ir al juego 
+            # Presionar cualquier tecla para ir al juego
             if event.type == pygame.KEYDOWN:
                 if K_a <= event.key <= K_z:
                     # Recordar los espacios en blanco! por eso el 6
@@ -275,7 +422,7 @@ def juego_terminado(puntos):
                         print(apodo)
                 if event.key == K_RETURN:
                     if len(str(apodo)) == 6:
-                        
+
                         write_in_record(puntos, apodo)
                         diccionario_record = recordsDic(records_helper()[0], records_helper()[1])
                         write_records(diccionario_record)
@@ -283,7 +430,7 @@ def juego_terminado(puntos):
 
                 if event.key == K_BACKSPACE:
                     apodo = apodo[0:len(apodo) - 2]
-            
+
             # if event.type == pygame.KEYDOWN:
             #     if event.key == K_RETURN:
             #         print("hello world")
@@ -293,7 +440,7 @@ def juego_terminado(puntos):
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
-        screen.blit(texto, (ANCHO / 2.5, 400)) 
+        screen.blit(texto, (ANCHO / 2.5, 400))
         fpsClock.tick(30)
         pygame.display.update()
 
@@ -309,13 +456,13 @@ def records():
         MENU_MOUSE_POSICION = pygame.mouse.get_pos()
         MENU_TEXT = get_font(30).render("", True, "#b68f40")
         MENU_RECT = MENU_TEXT.get_rect(center=(400, 100))
-        MENU_BUTTON = Button(None, pos=(400, 550), 
+        MENU_BUTTON = Button(None, pos=(400, 550),
                                 text_input="Volver al menu", font=get_font(25), base_color="#d7fcd4", hovering_color="White")
         screen.blit(MENU_TEXT, MENU_RECT)
         MENU_BUTTON.actualizar(screen)
 
         for event in pygame.event.get():
-            # Presionar cualquier tecla para ir al juego 
+            # Presionar cualquier tecla para ir al juego
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
@@ -323,7 +470,7 @@ def records():
                 if MENU_BUTTON.chequearEntrada(MENU_MOUSE_POSICION):
                     menu()
             MENU_BUTTON.actualizar(screen)
-            
+
             fpsClock.tick(30)
             pygame.display.flip()
 
@@ -348,4 +495,4 @@ def pintarDeNuevo(font, font2):
 
 # Programa Principal ejecuta Main
 if __name__ == "__main__":
-    juego_terminado(2)
+    main2()
